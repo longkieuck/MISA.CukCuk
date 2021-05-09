93236fb6e139ecf93118e9bbfaf8b27b32312d7e
longkieuck <longkieuck@gmail.com>
2021-05-10 06:44:03 +0700

    ui

---
+++ misacukcukui/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <TheNavbar/>
+    <TheHeader/>
+    <TheContent/>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue'
+import TheContent from './components/layout/TheContent.vue'
+import TheNavbar from './components/layout/TheNavbar.vue'
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    TheContent,
+    TheNavbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ misacukcukui/src/components/layout/TheNavbar.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="navbar">
+        <div class="logo-box">
+            <div class="toggle"></div>
+            <div class="logo"></div>
+        </div>
+        <div class="menu">
+            <div class="menu-item">
+                <div class="menu-item-logo dashboard"></div>
+                <div class="menu-item-title">Tổng quan</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo cash"></div>
+                <div class="menu-item-title">Tiền mặt</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo bank"></div>
+                <div class="menu-item-title">Tiền gửi</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo pu"></div>
+                <div class="menu-item-title">Mua hàng</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo sale"></div>
+                <div class="menu-item-title">Bán hàng</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo invoice"></div>
+                <div class="menu-item-title">Quản lý hoá đơn</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo stock"></div>
+                <div class="menu-item-title">Kho</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo tools"></div>
+                <div class="menu-item-title">Công cụ dụng cụ</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo assets"></div>
+                <div class="menu-item-title">Tài sản cố định</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo tax"></div>
+                <div class="menu-item-title">Thuế</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo price"></div>
+                <div class="menu-item-title">Giá thành</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo general"></div>
+                <div class="menu-item-title">Tổng hợp</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo budget"></div>
+                <div class="menu-item-title">Ngân sách</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo report"></div>
+                <div class="menu-item-title">Báo cáo</div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-logo finance"></div>
+                <div class="menu-item-title">Phân tích tài chính</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+                                                                                       
+    }
+</script>
+
+<style>
+@import '../../assets/css/layout/navbar.css';
+</style>
